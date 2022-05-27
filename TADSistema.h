9b2785e731dbbf8d9e03926b5d6b3d67acb2ddc4
ralphsenna <103199315+ralphsenna@users.noreply.github.com>
2022-05-27 18:03:38 -0300

    Commit Inicial

---
+++ TADSistema.h
@@ -0,0 +1,456 @@
+struct TpMensagem //578B
+{
+	TpMensagem *MensAnt; //4B
+	TpMensagem *MensProx; //4B
+	char Assunto[50], Mensagem[500], DataHora[20]; //50B + 500B + 20B = 570B
+};
+
+struct TpUsuario //93B
+{
+	TpUsuario *UsuAnt; //4B
+	TpUsuario *UsuProx; //4B
+	TpMensagem *UsuMens; //4B
+	char Login[50], Senha[30], Tipo; //50B + 30B + 1B = 81B
+};
+
+struct TpServidor //72B
+{
+	TpServidor *ServAnt; //4B
+	TpServidor *ServProx; //4B
+	TpUsuario *ServUsu; //4B
+	char Dominio[30], Local[30]; //30B + 30B = 60B
+};
+
+struct TpDescritor //13B
+{
+	char Raiz[5]; //5B
+	TpServidor *Inicio, *Fim; //4B + 4B = 8B
+};
+
+void Inicializar(TpDescritor &D)
+{
+	strcpy(D.Raiz, "Raiz");
+	D.Inicio = NULL;
+	D.Fim = NULL;
+}
+
+TpServidor* BuscaServidor(TpDescritor D, char Dominio[30], char Login[50])
+{
+	TpServidor *SAtual;
+	char *Div, LoginBackup[50];
+	strcpy(LoginBackup, Login);
+	SAtual = D.Inicio;
+	if (Dominio!="")
+	{
+		while (SAtual!=NULL && strcmp(SAtual->Dominio, Dominio)!=0)
+			SAtual = SAtual->ServProx;
+	}
+	else if (Login!="")
+	{
+		if (strchr(Login, '@'))
+		{
+			Div = strtok(Login, "@");
+			Div = strtok(NULL, "@");
+			while (SAtual!=NULL && strcmp(SAtual->Dominio, Div)!=0)
+				SAtual = SAtual->ServProx;
+			strcpy(Login, LoginBackup);
+		}
+		else
+			SAtual = NULL;
+	}
+	if (SAtual!=NULL)
+		return SAtual;
+	else
+		return NULL;
+}
+
+TpUsuario* BuscaUsuario(TpDescritor D, char Login[50])
+{
+	TpServidor *SAtual=BuscaServidor(D, "", Login);
+	TpUsuario *UAtual=SAtual->ServUsu;
+	char LoginBackup[50];
+	while (UAtual!=NULL && strcmp(UAtual->Login, Login)!=0)
+		UAtual = UAtual->UsuProx;
+	strcpy(Login, LoginBackup);
+	if (UAtual!=NULL)
+		return UAtual;
+	else
+		return NULL;
+}
+
+char VerificaLogin(TpDescritor D, TpUsuario U) 
+{
+	TpServidor *SAtual;
+	TpUsuario *UAtual;
+	char Tipo;
+	SAtual = BuscaServidor(D, "", U.Login);
+	if (SAtual!=NULL)
+	{
+		UAtual = SAtual->ServUsu;
+		while (UAtual!=NULL && strcmp(UAtual->Login, U.Login)!=0)
+			UAtual = UAtual->UsuProx;
+		if (strcmp(U.Login, UAtual->Login)==0 && strcmp(U.Senha, UAtual->Senha)==0)
+			Tipo = UAtual->Tipo;
+		else if (UAtual!=NULL && strcmp(U.Senha, UAtual->Senha)!=0)
+			Tipo ='S';
+	}
+	else
+		Tipo = 'X';
+	return Tipo;
+}
+
+char SeparaDominio(TpDescritor D, char Login[50])
+{
+	TpServidor *Aux=D.Inicio;
+	char *Div, LoginBackup[50];
+	strcpy(LoginBackup, Login);
+	if (strchr(Login, '@'))
+	{
+		Div = strtok(Login, "@");
+		Div = strtok(NULL, "@");
+		while (Aux!=NULL && strcmp(Aux->Dominio, Div)!=0)
+			Aux = Aux->ServProx;
+		strcpy(Login, LoginBackup);
+		if (Aux!=NULL)
+			return 'T';
+		else
+			return 'F';
+	}
+	else
+		return 'L';
+}
+
+void LimparMensagens(TpUsuario *Usu)
+{
+	TpMensagem *MAuxF;
+	MAuxF = Usu->UsuMens;
+	if (MAuxF!=NULL)
+	{
+		while (MAuxF->MensProx!=NULL)
+			MAuxF = MAuxF->MensProx;
+		while (MAuxF->MensAnt!=NULL)
+		{
+			MAuxF = MAuxF->MensAnt;
+			delete(MAuxF->MensProx);
+		}
+		delete(MAuxF);
+		Usu->UsuMens = NULL;
+	}
+}
+
+void LimparUsuarios(TpServidor *Serv)
+{
+	TpUsuario *UAuxF;
+	UAuxF = Serv->ServUsu;
+	if (UAuxF!=NULL)
+	{
+		while (UAuxF->UsuProx!=NULL)
+			UAuxF = UAuxF->UsuProx;
+		while (UAuxF->UsuAnt!=NULL)
+		{
+			//LimparMensagens(UAuxF);
+			UAuxF = UAuxF->UsuAnt;
+			delete(UAuxF->UsuProx);
+		}
+		delete(UAuxF);
+		Serv->ServUsu = NULL;
+	}
+}
+
+void CadastrarServidorOrd(TpDescritor &D, TpServidor Serv)
+{
+	TpServidor *Novo, *Aux;
+	Novo = new TpServidor;
+	strcpy(Novo->Dominio, Serv.Dominio);
+	strcpy(Novo->Local, Serv.Local);
+	Novo->ServProx = NULL;
+	Novo->ServAnt = NULL;
+	Novo->ServUsu = NULL;
+	if (D.Inicio==NULL)
+	{
+		D.Inicio = Novo;
+		D.Fim = Novo;
+	}
+	else
+	{
+		if (strcmp(Serv.Dominio, D.Inicio->Dominio)<=0)
+		{
+			D.Inicio->ServAnt = Novo;
+			Novo->ServProx = D.Inicio;
+			D.Inicio = Novo;	
+		}
+		else if (strcmp(Serv.Dominio, D.Fim->Dominio)>=0)
+		{
+			D.Fim->ServProx = Novo;
+			Novo->ServAnt = D.Fim;
+			D.Fim = Novo;
+		}
+		else
+		{
+			Aux = D.Inicio;
+			while (strcmp(Serv.Dominio, Aux->ServProx->Dominio)>0)
+                Aux = Aux->ServProx;
+			Novo->ServProx = Aux->ServProx;
+			Novo->ServAnt = Aux;
+			Aux->ServProx = Novo;
+			Aux->ServProx->ServAnt = Novo;
+		}
+	}
+}
+
+void ListarServidores(TpDescritor D)
+{
+	TpServidor *Aux=D.Inicio;
+	if (Aux==NULL)
+		printf("\n\nLista de Servidores Vazia!\n");
+	else
+	{
+		printf("\n\n  ** LISTA DE TODOS OS SERVIDORES **\n");
+		printf("-----------------------------------------------\n");
+		printf("| Dominio                 | Local             |\n");
+		printf("-----------------------------------------------\n");
+		printf("|                         |                   |\n");
+		while (Aux!=NULL)
+		{
+			printf("| %-23s | %-17s |\n", Aux->Dominio, Aux->Local);
+			Aux = Aux->ServProx;
+		}
+		printf("-----------------------------------------------\n");
+		getch();
+	}
+}
+
+void ConsultarServidores(TpDescritor D)
+{
+	TpServidor *Aux=D.Inicio;
+	char Dominio[30];
+	if (Aux==NULL)
+		printf("\n\nLista de Servidores Vazia!\n");
+	else
+	{
+		printf("\n\nQual nome do Dominio do Servidor que procura?\n");
+		gets(Dominio);
+		while (strcmp(Dominio, "\0")!=0)
+		{
+			Aux = BuscaServidor(D, Dominio, "");
+			if (Aux!=NULL)
+				printf("\nDominio: %s\tLocal: %s\n", Aux->Dominio, Aux->Local);
+			else
+				printf("\nEste Servidor nao esta cadastrado!\n");
+			getch();
+			printf("\nQual nome do Dominio do Servidor que procura?\n");
+			gets(Dominio);
+		}
+	}
+}
+
+void ExcluirServidor(TpDescritor &D, char Dominio[30])
+{
+	TpServidor *Aux;
+	if (strcmp(D.Inicio->Dominio, Dominio)==0)
+	{
+		Aux = D.Inicio;
+		LimparUsuarios(Aux);
+		D.Inicio = D.Inicio->ServProx;
+		D.Inicio->ServAnt = NULL;
+		delete(Aux);
+	}
+	else if (strcmp(D.Fim->Dominio, Dominio)==0)
+	{
+		Aux = D.Fim;
+		LimparUsuarios(Aux);
+		D.Fim = D.Fim->ServAnt;
+		D.Fim->ServProx = NULL;
+		delete(Aux);
+	}
+	else
+	{
+		Aux = D.Inicio;
+		while (strcmp(Aux->Dominio, Dominio)!=0)
+			Aux = Aux->ServProx;	
+		LimparUsuarios(Aux);
+		Aux->ServProx->ServAnt = Aux->ServAnt;
+		Aux->ServAnt->ServProx = Aux->ServProx;
+		delete(Aux);
+	}
+}
+
+void CadastrarUsuarioOrd(TpDescritor &D, TpUsuario Usu)
+{
+	TpUsuario *Novo, *Aux;
+	TpServidor *Serv=BuscaServidor(D, "", Usu.Login);
+	Novo = new TpUsuario;
+	strcpy(Novo->Login, Usu.Login);
+	strcpy(Novo->Senha, Usu.Senha);
+	Novo->Tipo = Usu.Tipo;
+	Novo->UsuProx = NULL;
+	Novo->UsuAnt = NULL;
+	Novo->UsuMens = NULL;
+	if (Serv->ServUsu==NULL)
+		Serv->ServUsu = Novo;
+	else if (strcmp(Novo->Login, Serv->ServUsu->Login)<=0)
+	{
+		Novo->UsuProx = Serv->ServUsu;
+		Novo->UsuProx->UsuAnt = Novo;
+		Serv->ServUsu = Novo;	
+	}
+	else
+	{
+		Aux = Serv->ServUsu->UsuProx;
+		while (Aux->UsuProx!=NULL && strcmp(Novo->Login, Aux->Login)>0)
+			Aux = Aux->UsuProx;
+		if (strcmp(Novo->Login, Aux->Login)<=0)
+		{
+			Novo->UsuProx = Aux;
+			Novo->UsuAnt = Aux->UsuAnt;
+			Aux->UsuAnt = Novo;
+			Novo->UsuAnt->UsuProx = Novo;	
+		}
+		else
+		{
+			Novo->UsuAnt = Aux;
+			Aux->UsuProx = Novo;
+		}
+	}
+}
+
+void ListarUsuarios(TpDescritor D)
+{
+	TpServidor *ServAux;
+	TpUsuario *UsuAux;
+	char Dominio[30], Resp;
+	if (D.Inicio==NULL)
+		printf("\n\nLista de Servidores Vazia!\n");
+	else
+	{
+		printf("\n\nTodos os Usuarios do Sistema (T) ou Apenas os Usuarios de um Servidor Especifico (E)?\n");
+		if (toupper(getch())=='T')
+		{
+			ServAux = D.Inicio;
+			printf("\n\t** LISTA DE TODOS OS USUARIOS **\n");
+			while (ServAux!=NULL)
+			{
+				UsuAux = ServAux->ServUsu;
+				if (UsuAux!=NULL)
+				{
+					printf("-----------------------------------------\n");
+					printf("| Dominio: %-20s |\n", ServAux->Dominio);
+					printf("-----------------------------------------\n");
+					printf("| Login do Usuario              | Tipo  |\n");
+					printf("-----------------------------------------\n");
+					printf("|                               |       |\n");
+					while (UsuAux!=NULL)
+					{
+						printf("| %-29s | %-5c |\n", UsuAux->Login, UsuAux->Tipo);
+						UsuAux = UsuAux->UsuProx;
+					}
+					printf("-----------------------------------------\n\n");
+				}
+				ServAux = ServAux->ServProx;
+			}
+		}
+		else
+		{
+			printf("\nQual Servidor?\n");
+			gets(Dominio);
+			ServAux = BuscaServidor(D, Dominio, "");
+			if (ServAux!=NULL)
+			{
+				UsuAux = ServAux->ServUsu;
+				if (UsuAux!=NULL)
+				{
+					printf("\n\n\t** LISTA DE USUARIOS DO SERVIDOR \"%s\" **\n", Dominio);
+					printf("---------------------------------\n");
+					printf("| Dominio: %-20s |\n", Dominio);
+					printf("---------------------------------\n");
+					printf("| Login do Usuario              |\n");
+					printf("---------------------------------\n");
+					printf("|                               |\n");
+					while (UsuAux!=NULL)
+					{
+						printf("| %-29s |\n", UsuAux->Login);
+						UsuAux = UsuAux->UsuProx;
+					}
+					printf("---------------------------------\n");
+				}
+				else
+					printf("\nServidor nao contem Usuarios!\n");
+			}
+			else
+				printf("\nServidor nao existe!\n");
+		}
+		getch();
+	}
+}
+
+//Não terminado
+TpUsuario* ExcluirUsuario(TpUsuario *Usu,char Login[50])
+{
+	TpUsuario *UsuAux;
+	UsuAux = Usu;
+	if(strcmp(UsuAux->Login,Login)==0)
+	{
+		Usu->UsuProx->UsuAnt = Usu;
+		Usu = Usu->UsuProx;
+		delete(UsuAux);
+	}
+	else
+	{
+		while(UsuAux->UsuProx!=NULL && strcmp(UsuAux->Login,Login)!=0)
+			UsuAux = UsuAux -> UsuProx;
+		if(strcmp(UsuAux->Login,Login)==0)
+		{
+			if(UsuAux->UsuProx==NULL)
+			{
+				LimparMensagens(UsuAux);
+				UsuAux->UsuAnt->UsuProx = NULL;
+			}
+			else
+			{
+				LimparMensagens(UsuAux);
+				UsuAux->UsuProx->UsuAnt = UsuAux->UsuAnt;
+				UsuAux->UsuAnt->UsuProx = UsuAux->UsuProx;
+			}
+			delete(UsuAux);
+			printf("Usuario deletado\n");
+		}
+		else
+		{
+			printf("Usuario nao encontrado\n");
+		}
+	}
+	return Usu;
+}
+
+//Não terminado
+TpMensagem* ExcluirMensagem(char Assunto[50],TpMensagem *Mens)
+{
+	TpMensagem *Aux;
+	Aux = Mens;
+	if(strcmp(Assunto,Mens->Assunto)==0)
+	{
+		Mens = Mens->MensProx;
+		delete(Aux);
+	}
+	else
+	{
+		while(Aux->MensProx!=NULL && strcmp(Aux->Assunto,Assunto)!=0)
+			Aux = Aux->MensProx;
+		if(strcmp(Aux->Assunto,Assunto)==0)
+		{
+			if(Aux->MensProx==NULL)
+			{
+				Aux->MensAnt->MensProx = NULL;
+				delete(Aux);
+			}
+			Aux->MensProx->MensAnt = Aux->MensAnt;
+			Aux->MensAnt->MensProx = Aux->MensProx;
+			delete(Aux);
+		}
+		else
+		{
+			printf("Assunto não encontrado\n");	
+		}	
+	}
+	return Mens;
+}
---
+++ Trabalho - Sistema de Mensagens.cpp
@@ -0,0 +1,462 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <ctype.h>
+#include <windows.h>
+#include "TADSistema.h"
+
+
+//Não terminado (Mensagens não incluídas)
+void RecuperaListas(TpDescritor &D, FILE *ArqServ, FILE *ArqUsu, FILE *ArqMens, TpServidor *Serv, TpUsuario *Usu, TpMensagem *Mens)
+{
+	TpServidor SAtual;
+	TpUsuario UAtual;
+	//TpMensagem MAtual;
+	rewind(ArqServ);
+	rewind(ArqUsu);
+	//rewind(ArqMens);
+	ArqServ = fopen("Servidores.dat", "rb");
+	ArqUsu = fopen("Usuarios.dat", "rb");
+	//ArqMens = fopen("Mensagens", "rb");
+	fread(&SAtual, sizeof(TpServidor), 1, ArqServ);
+	while (!feof(ArqServ))
+	{
+		Serv = new TpServidor;
+		strcpy(Serv->Dominio, SAtual.Dominio);
+		strcpy(Serv->Local, SAtual.Local);
+		Serv->ServAnt = NULL;
+		Serv->ServProx = NULL;
+		Serv->ServUsu = NULL;
+		if (D.Inicio==NULL)
+		{
+			D.Inicio = Serv;
+			D.Fim = Serv;
+		}
+		else
+		{
+			D.Fim->ServProx = Serv;
+			Serv->ServAnt = D.Fim;
+			D.Fim = Serv;
+		}
+		fread(&UAtual, sizeof(TpUsuario), 1, ArqUsu);
+		while (!feof(ArqUsu) && BuscaServidor(D, "", UAtual.Login)!=NULL)
+		{
+			Usu = new TpUsuario;
+			strcpy(Usu->Login, UAtual.Login);
+			strcpy(Usu->Senha, UAtual.Senha);
+			Usu->Tipo = UAtual.Tipo;
+			Usu->UsuAnt = NULL;
+			Usu->UsuProx = NULL;
+			Usu->UsuMens = NULL;
+			if (D.Fim->ServUsu==NULL)
+				D.Fim->ServUsu = Usu;
+			else
+			{
+				D.Fim->ServUsu->UsuProx = Usu;
+				Usu->UsuAnt = D.Fim->ServUsu;
+			}			
+			fread(&UAtual, sizeof(TpUsuario), 1, ArqUsu);
+		}
+		fread(&SAtual, sizeof(TpServidor), 1, ArqServ);
+	}
+}
+
+//Não terminado (Mensagens não incluídas)
+void SalvarListas(TpDescritor &D, TpServidor *Serv, TpUsuario *Usu, TpMensagem *Mens)
+{
+	FILE *ArqServ = fopen("Servidores.dat", "wb");
+	FILE *ArqUsu = fopen("Usuarios.dat", "wb");
+	//FILE *ArqMens = fopen("Mensagens.dat", "wb");
+	fseek(ArqServ, 0, 2);
+	fseek(ArqUsu, 0, 2);
+	//fseek(ArqMens, 0, 2);
+	TpServidor SAtual;
+	TpUsuario UAtual;
+	//TpMensagem MAtual;
+	Serv = D.Inicio;
+	while (Serv!=NULL)
+	{
+		strcpy(SAtual.Dominio, Serv->Dominio);
+		strcpy(SAtual.Local, Serv->Local);
+		SAtual.ServAnt = NULL;
+		SAtual.ServProx = NULL;
+		SAtual.ServUsu = NULL;
+		fwrite(&SAtual, sizeof(TpServidor), 1, ArqServ);
+		Usu = Serv->ServUsu;
+		if (Usu==NULL)
+		{
+			strcpy(UAtual.Login, "");
+			fwrite(&UAtual, sizeof(TpUsuario), 1, ArqUsu);
+		}
+		else
+		{
+			while (Usu!=NULL)
+			{
+				strcpy(UAtual.Login, Usu->Login);
+				strcpy(UAtual.Senha, Usu->Senha);
+				UAtual.Tipo = Usu->Tipo;
+				UAtual.UsuAnt = NULL;
+				UAtual.UsuProx = NULL;
+				UAtual.UsuMens = NULL;
+				fwrite(&UAtual, sizeof(TpUsuario), 1, ArqUsu);
+				Usu = Usu->UsuProx;
+			}
+			strcpy(UAtual.Login, "");
+			fwrite(&UAtual, sizeof(TpUsuario), 1, ArqUsu);
+		}
+		Serv = Serv->ServProx;
+	}
+	fclose(ArqServ);
+	fclose(ArqUsu);
+	//fclose(ArqMens);
+}
+
+void AbrindoPrograma(TpDescritor &D, TpServidor *Serv, TpUsuario *Usu, TpMensagem *Mens)
+{
+	FILE *ArqServ = fopen("Servidores.dat", "rb");
+	FILE *ArqUsu = fopen("Usuarios.dat", "rb");
+	FILE *ArqMens = fopen("Mensagens.dat", "rb");
+	TpServidor SAtual;
+	TpUsuario UAtual;
+	if (ArqServ==NULL)
+	{
+		ArqServ = fopen("Servidores.dat", "ab");
+		ArqUsu = fopen("Usuarios.dat", "ab");
+		strcpy(UAtual.Login, "radmin@admin");
+		strcpy(UAtual.Senha, "123");
+		UAtual.Tipo = 'A';
+		UAtual.UsuAnt = NULL;
+		UAtual.UsuProx = NULL;
+		UAtual.UsuMens = NULL;
+		fwrite(&UAtual, sizeof(TpUsuario), 1, ArqUsu);
+		strcpy(SAtual.Dominio, "admin");
+		strcpy(SAtual.Local, "Meu Computador");
+		SAtual.ServAnt = NULL;
+		SAtual.ServProx = NULL;
+		SAtual.ServUsu = NULL;
+		fwrite(&SAtual, sizeof(TpServidor), 1, ArqServ);
+		strcpy(SAtual.Dominio, "hotmail.com");
+		strcpy(SAtual.Local, "Brasil");
+		SAtual.ServAnt = NULL;
+		SAtual.ServProx = NULL;
+		SAtual.ServUsu = NULL;
+		fwrite(&SAtual, sizeof(TpServidor), 1, ArqServ);
+		RecuperaListas(D, ArqServ, ArqUsu, ArqMens, Serv, Usu, Mens);
+	}
+	else
+		RecuperaListas(D, ArqServ, ArqUsu, ArqMens, Serv, Usu, Mens);
+	fclose(ArqServ);
+	fclose(ArqUsu);
+	fclose(ArqMens);
+}
+
+//Busca Usuario no Servidor igual Dominio de seu Login
+char VerificaLogin(TpDescritor D, TpUsuario U) 
+{
+	TpServidor *SAtual;
+	TpUsuario *UAtual;
+	char Tipo;
+	SAtual = BuscaServidor(D, "", U.Login);
+	if (SAtual!=NULL)
+	{
+		UAtual = SAtual->ServUsu;
+		while (UAtual!=NULL && strcmp(UAtual->Login, U.Login)!=0)
+			UAtual = UAtual->UsuProx;
+		if (strcmp(U.Login, UAtual->Login)==0 && strcmp(U.Senha, UAtual->Senha)==0)
+			Tipo = UAtual->Tipo;
+		else if (UAtual!=NULL && strcmp(U.Senha, UAtual->Senha)!=0)
+			Tipo ='S';
+	}
+	else
+		Tipo = 'X';
+	return Tipo;
+}
+
+void CadastroServidores(TpDescritor &D)
+{
+	TpServidor Serv;
+	printf("\n\n** CADASTRO DE SERVIDORES **\n\n");
+	printf("Dominio do Servidor: ");
+	fflush(stdin);
+	gets(Serv.Dominio);
+	while (strcmp(Serv.Dominio, "\0")!=0)
+	{
+		if (!BuscaServidor(D, Serv.Dominio, ""))
+		{
+			printf("Localizacao: ");
+			fflush(stdin);
+			gets(Serv.Local);
+			CadastrarServidorOrd(D, Serv);
+			printf("\nServidor %s cadastrado com sucesso!\n", Serv.Dominio);
+		}
+		else
+			printf("\n\nServidor ja existente!\n");
+		getch();
+		printf("\nDominio do Servidor: ");
+		fflush(stdin);
+		gets(Serv.Dominio);
+	}
+}
+
+//Não terminado
+void CadastroUsuarios(TpDescritor &D, char Tipo)
+{
+	TpUsuario Usu;
+	char Resp;
+	printf("\n\n** CADASTRO DE USUARIOS **\n\n");
+	printf("Login do Usuario: ");
+	fflush(stdin);
+	gets(Usu.Login);
+	while (strcmp(Usu.Login, "\0")!=0)
+	{
+		Resp = SeparaDominio(D, Usu.Login);
+		if (Resp=='T')
+		{
+			if(!BuscaUsuario(D, Usu.Login))
+			{
+				printf("Senha: ");
+				fflush(stdin);
+				gets(Usu.Senha);
+				if (Tipo=='A')
+				{
+					printf("Tipo de Usuario (A) Administrador ou (C) Comum: ");
+					Usu.Tipo = toupper(getche());
+				}
+				else
+					Usu.Tipo = 'C';
+				CadastrarUsuarioOrd(D, Usu);
+				printf("\n\nUsuario %s cadastrado com sucesso!\n", Usu.Login);
+			}
+			else
+				printf("\nUsuario ja existente!\n");
+		}
+		else if (Resp=='L')
+			printf("\nLogin Invalido!\n");
+		else
+			printf("\nDominio nao existente!\n");
+		getch();
+		printf("\nLogin do Usuario: ");
+		fflush(stdin);
+		gets(Usu.Login);
+	}
+}
+
+void ExclusaoServidores(TpDescritor &D)
+{
+	TpServidor *Aux;
+	char Dominio[30];
+	printf("\n\n** EXCLUSAO DE SERVIDORES **\n\n");
+	printf("Qual Servidor deseja excluir?\n");
+	gets(Dominio);
+	while (strcmp(Dominio, "\0")!=0)
+	{
+		if (strcmp(Dominio, "admin")!=0)
+		{
+			if (Aux!=NULL)
+			{
+				ExcluirServidor(D, Dominio);
+				printf("\nServidor %s excluido!\n", Dominio);
+			}
+			else
+				printf("\nEste Servidor nao esta cadastrado!\n");
+			getch();
+			printf("\nQual Servidor deseja excluir?\n");
+			gets(Dominio);
+		}
+		else
+			printf("\nO servidor admin nao pode ser excluido!\n");
+		getch();
+		printf("\nQual Servidor deseja excluir?\n");
+		gets(Dominio);
+	}
+}
+
+//Não terminado
+char MenuUsuario(char Login[50])
+{
+	system("cls");
+	printf("** MENU DO USUARIO **\n");
+	printf("Usuario: %s\t\tComum\n\n", Login);
+	printf("[A]	Enviar Mensagens\n");
+	printf("[B] Listar Mensagens\n");
+	printf("[C] Buscar Mensagens\n");
+	printf("[D] Deletar Mensagens\n");
+
+	printf("\n[E] Listar Usuarios\n");
+	printf("[F] Buscar Usuarios\n");
+	printf("[G] Alterar Usuario\n");
+	printf("[H] Deletar Usuario\n");
+
+	printf("\n[ESC] Sair do Programa\n");
+	printf("\nPressione a opcao desejada: ");
+	return toupper(getche());
+}
+
+char MenuAdmin(char Login[50])
+{
+	system("cls");
+	printf("** MENU DO ADMINISTRADOR **\n");
+	printf("Usuario: %s\t\tAdministrador\n\n", Login);
+	printf("[A] Cadastrar Servidores\n");
+	printf("[B] Listar Servidores\n");
+	printf("[C] Consultar Servidores\n");
+	printf("[D] Alterar Servidores\n");
+	printf("[E] Deletar Servidores\n");
+
+	printf("\n[F] Cadastrar Usuarios\n");
+	printf("[G] Listar Usuarios\n");
+	printf("[H] Consultar Usuarios\n");
+	printf("[I] Alterar Usuarios\n");
+	printf("[J] Deletar Usuarios\n");
+
+	printf("\n[ESC] Sair do Programa\n");
+	printf("\nPressione a opcao desejada: ");
+	return toupper(getche());
+}
+
+void MenuLogin(TpDescritor &D)
+{
+	TpUsuario Usu;
+	char Tipo, Opcao;
+	system("cls");
+	printf("\t\t** LOGIN DO USUARIO **\n\n");
+	printf("Login: ");
+	gets(Usu.Login);
+	while (strcmp(Usu.Login, "\0")!=0)
+	{
+		printf("Senha: ");
+		gets(Usu.Senha);
+		Tipo = VerificaLogin(D, Usu);
+		if (Tipo=='C')
+			do
+			{
+				Opcao = MenuUsuario(Usu.Login);
+				switch(Opcao)
+				{
+					case 'A':
+							break;
+
+					case 'B':
+							break;
+
+					case 'C':
+							break;
+
+					case 'D':
+							break;
+
+					case 'E':
+							break;
+
+					case 'F':
+							break;
+
+					case 'G':
+							break;
+
+					case 'H':
+							break;
+				}
+			}while(Opcao!=27);
+		else if (Tipo=='A')
+			do
+			{
+				Opcao = MenuAdmin(Usu.Login);
+				switch(Opcao)
+				{
+					case 'A':
+							CadastroServidores(D);
+							break;
+
+					case 'B':
+							ListarServidores(D);
+							break;
+
+					case 'C':
+							ConsultarServidores(D);
+							break;
+
+					case 'D':
+							//AlterarServidores();
+							break;
+
+					case 'E':
+							ExclusaoServidores(D);
+							break;
+
+					case 'F':
+							CadastroUsuarios(D, Tipo);
+							break;
+
+					case 'G':
+							ListarUsuarios(D);
+							break;
+
+					case 'H':
+							//ConsultarUsuarios();
+							break;		
+
+					case 'I':
+							//AlterarUsuarios();
+							break;	
+
+					case 'J':
+							//ExcluirUsuarios();
+							break;	
+				}
+			}while(Opcao!=27);
+		else if (Tipo=='S')
+		{
+			printf("\nSenha Incorreta!");
+			getch();
+		}	
+		else
+		{
+			printf("\nUsuario Inexiste!");
+			getch();
+		}
+		system("cls");
+		printf("\t\t** LOGIN DO USUARIO **\n\n");
+		printf("Login: ");
+		gets(Usu.Login);
+	}
+}
+
+char MenuInicial(void)
+{
+	system("cls");
+	printf("\t\t** SISTEMA DE MENSAGENS **\n\n");
+	printf("### Menu de Opcoes ###\n");
+	printf("[A] Cadastrar Usuario\n");
+	printf("[B] Login de Usuario\n");
+	printf("\n[ESC] Sair do Programa\n");
+	printf("\nPressione a opcao desejada: ");
+	return toupper(getche());
+}
+
+int main(void)
+{
+	TpDescritor Descritor;
+	TpServidor *Servidores;
+	TpUsuario *Usuarios;
+	TpMensagem *Mensagens;
+	char Opcao;
+	Inicializar(Descritor);
+	AbrindoPrograma(Descritor, Servidores, Usuarios, Mensagens);
+	do
+	{
+		Opcao = MenuInicial();
+		switch(Opcao)
+		{
+			case 'A':
+					CadastroUsuarios(Descritor, 'C');
+					break;
+
+			case 'B':
+					MenuLogin(Descritor);
+					break;
+		}
+	}while(Opcao!=27);
+	SalvarListas(Descritor, Servidores, Usuarios, Mensagens);
+	return 0;
+}
